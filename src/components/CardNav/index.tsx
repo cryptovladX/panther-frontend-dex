81ab428dc10473e27f47d0e480bfcb57498cca76
cryptovladX <94879679+cryptovladX@users.noreply.github.com>
2021-11-23 10:32:48 +0700

    Update index.tsx

--- src/components/CardNav/index.tsx
+++ src/components/CardNav/index.tsx
@@ -3,55 +3,12 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { ButtonMenu, ButtonMenuItem } from '@pantherswap-libs/uikit'
 import TranslatedText from '../TranslatedText'
-import { View, Image, StyleSheet } from 'react-native'
 
 const StyledNav = styled.div`
   margin-bottom: 40px;
 `
 
-
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-  logo: {
-    width: 66,
-    height: 58,
-  },
-});
-
-const DisplayAnImage = () => {
-  return (
-    <View style={styles.container}>
-      <Image
-        style={styles.tinyLogo}
-        source={require('@expo/snack-static/react-native-logo.png')}
-      />
-      <Image
-        style={styles.tinyLogo}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
-      />
-      <Image
-        style={styles.logo}
-        source={{
-          uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
-        }}
-      />
-    </View>
-  );
-}
-
-export default DisplayAnImage;
-
 const Nav = ({ activeIndex = 0 }: { activeIndex?: number }) => (
-  
   
   <StyledNav>
     <ButtonMenu activeIndex={activeIndex} size="sm" variant="subtle">
